bb44eb45fc408b20c392a3e49e0fc373b2c7599c
Arthur Judd <amjudd315@gmail.com>
2022-04-28 08:58:58 -0600

    widget-loader recognizes multiple anchors

--- vue_widgets/widget-loader.js
+++ vue_widgets/widget-loader.js
@@ -1,23 +1,10 @@
-function insertWidget(widgetName,elId,config) {
-    let anchorEl = document.getElementById(elId);
-    let anchorId = `${widgetName}_${elId}`;
-    anchorEl.outerHTML = /*html*/`
-        <div id="${anchorId}"><${widgetName} :config="config" /></div>
-    `
-    new Vue({el: "#"+anchorId, data:{config}});
+function insertWidget(widgetName,elClass,config) {
+    let anchors = document.querySelectorAll("."+elClass);
+    anchors.forEach( (el,i) => {
+        let anchorId = `${widgetName}-${elClass}-${i}`;
+        el.outerHTML = /*html*/`
+            <div id="${anchorId}"><${widgetName} :config="config" /></div>
+        `
+        new Vue({el: "#"+anchorId, data:{config}});
+    })
 }
-
-document.head.insertAdjacentHTML("beforeend", /* html */`
-<style>
-.st-widget-placeholder {
-}
-
-.st-widget-placeholder::before {
-    content: 'ST Widget';
-    color: #888;
-    border: 2px dashed;
-    text-transform: uppercase;
-    padding: .5em;
-}
-</style>
-`);
